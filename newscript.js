c3c954712d372bdcb4791f43dab61e95533f2424
JoshuaKomala <joshuakomala@gmail.com>
2017-01-12 23:30:32 -0800

    NEW WEBSITE

---
+++ newscript.js
@@ -0,0 +1,23 @@
+// LINKS
+
+$("#foodforthought").on("click",function(){
+	window.open("http://food-4-thought.herokuapp.com")
+});
+$("#jpl").on("click",function(){
+	window.open("http://www.jpl.nasa.gov/")
+});
+$("#fb").on("click",function(){
+	window.open("https://www.facebook.com/joshua.komala")
+});
+
+$("#insta").on("click",function(){
+	window.open("https://www.instagram.com/joshkiddin/")
+});
+
+$("#git").on("click",function(){
+	window.open("https://github.com/JoshuaKomala/")
+});
+
+$("#linkedin").on("click",function(){
+	window.open("https://www.linkedin.com/in/julianjoshuakomala")
+});
