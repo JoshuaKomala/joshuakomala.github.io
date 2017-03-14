62c7d2c84c83eba8d7497c692810df52c601ae27
Joshua Komala <joshuakomala@gmail.com>
2017-03-14 12:49:31 -0700

    Added Hobbies and 404

--- newscript.js
+++ newscript.js
@@ -1,23 +1,26 @@
 // LINKS
 
 $("#foodforthought").on("click",function(){
-	window.open("http://food-4-thought.herokuapp.com")
+	window.open("http://food-4-thought.herokuapp.com");
 });
 $("#jpl").on("click",function(){
-	window.open("http://www.jpl.nasa.gov/")
+	window.open("http://www.jpl.nasa.gov/");
 });
 $("#fb").on("click",function(){
-	window.open("https://www.facebook.com/joshua.komala")
+	window.open("https://www.facebook.com/joshua.komala");
 });
 
 $("#insta").on("click",function(){
-	window.open("https://www.instagram.com/joshkiddin/")
+	window.open("https://www.instagram.com/joshkiddin/");
 });
 
 $("#git").on("click",function(){
-	window.open("https://github.com/JoshuaKomala/")
+	window.open("https://github.com/JoshuaKomala/");
 });
 
 $("#linkedin").on("click",function(){
-	window.open("https://www.linkedin.com/in/julianjoshuakomala")
+	window.open("https://www.linkedin.com/in/julianjoshuakomala");
+});
+$("#twitch").on("click",function(){
+	window.open("https://www.twitch.tv/shyyyyk");
 });
